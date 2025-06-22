786d2ca26b7de3d02a9bc81e39399a45a0ab4736
VladimirKobranov <josephclturok@gmail.com>
2025-06-22 23:39:00 +0900

    init

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,56 @@
-import resolve from '@rollup/plugin-node-resolve';
-import copy from 'rollup-plugin-copy';
+import resolve from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
 
-export default [{
-    input: 'src/index.js',
+export default [
+  {
+    input: "src/index.js",
     output: {
-        dir: 'dist',
-        format: 'esm',
-        sourcemap: true
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
     },
     plugins: [
-        resolve(),
-        copy({
-            targets: [{
-                src: 'src/index.html',
-                dest: 'dist',
-                transform: (contents) => {
-                    return contents.toString().replace('<base href="">', `<base href="${process.env.BASE_HREF ?? ''}">`);
-                }
-            }, {
-                src: 'src/index.css',
-                dest: 'dist'
-            }]
-        })
-    ]
-}];
+      resolve(),
+      copy({
+        targets: [
+          {
+            src: "src/index.html",
+            dest: "dist",
+            transform: (contents) => {
+              return contents
+                .toString()
+                .replace(
+                  '<base href="">',
+                  `<base href="${process.env.BASE_HREF ?? ""}">`,
+                );
+            },
+          },
+          {
+            src: "assets/settings.json",
+            dest: "dist",
+          },
+          {
+            src: "src/index.css",
+            dest: "dist",
+          },
+          {
+            src: "assets/first.ply",
+            dest: "dist",
+          },
+          {
+            src: "assets/sec.ply",
+            dest: "dist",
+          },
+          {
+            src: "assets/third.ply",
+            dest: "dist",
+          },
+          {
+            src: "assets/quad.ply",
+            dest: "dist",
+          },
+        ],
+      }),
+    ],
+  },
+];
